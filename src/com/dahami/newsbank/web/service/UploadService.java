166da5d714a765db613b29e8bfbfc64dc72f3f0e
tealight <tealight@dahami.com>
2017-11-30 15:46:17 +0900

    주소변경

--- src/com/dahami/newsbank/web/service/UploadService.java
+++ src/com/dahami/newsbank/web/service/UploadService.java
@@ -37,17 +37,17 @@ import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 public class UploadService extends ServiceBase {
 
 	public static final long serialVersionUID = 1L;
-/*	public static final String PATH_COMP_DOC_BASE = "/data/newsbank/comp/doc";
+	public static final String PATH_COMP_DOC_BASE = "/data/newsbank/comp/doc";
 	public static final String PATH_COMP_BANK_BASE = "/data/newsbank/comp/bank";
 	public static final String PATH_COMP_CONTRACT_BASE = "/data/newsbank/comp/contract";
 	public static final String PATH_LOGO_BASE = "/data/newsbank/logo";
-	public static final String PATH_BASE = "/data/newsbank/tmp";*/
+	public static final String PATH_BASE = "/data/newsbank/tmp";
 
-	public static final String PATH_COMP_DOC_BASE = "D:/IdeaProjects/git/newsbank/comp/doc";
-	public static final String PATH_COMP_BANK_BASE = "D:/IdeaProjects/git/newsbank/comp/bank";
-	public static final String PATH_COMP_CONTRACT_BASE =  "D:/IdeaProjects/git/newsbank/comp/contract";
-	public static final String PATH_LOGO_BASE = "D:/IdeaProjects/git/newsbank/logo";
-	public static final String PATH_BASE = "D:/IdeaProjects/git/newsbank/tmp";
+//	public static final String PATH_COMP_DOC_BASE = "D:/IdeaProjects/git/newsbank/comp/doc";
+//	public static final String PATH_COMP_BANK_BASE = "D:/IdeaProjects/git/newsbank/comp/bank";
+//	public static final String PATH_COMP_CONTRACT_BASE =  "D:/IdeaProjects/git/newsbank/comp/contract";
+//	public static final String PATH_LOGO_BASE = "D:/IdeaProjects/git/newsbank/logo";
+//	public static final String PATH_BASE = "D:/IdeaProjects/git/newsbank/tmp";
 
 	/*
 	 * (non-Javadoc)
