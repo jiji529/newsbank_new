dc711c55d4cf036591023c5d048ba4bbe0a6684a
최성현 <tealight@dahami.com>
2018-04-10 14:38:31 +0900

    결제 정보 업데이트 쿼리 수정

--- src/com/dahami/newsbank/web/servlet/PurchaseJSON.java
+++ src/com/dahami/newsbank/web/servlet/PurchaseJSON.java
@@ -51,7 +51,8 @@ public class PurchaseJSON extends NewsbankServletBase {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.setContentType("application/json;charset=UTF-8");
 		request.setCharacterEncoding("UTF-8");
@@ -71,8 +72,7 @@ public class PurchaseJSON extends NewsbankServletBase {
 
 		Map<String, Object> LGD_DATA = new HashMap<String, Object>();
 
-		
-		List<PaymentDetailDTO> DetailList =new ArrayList<PaymentDetailDTO>();
+		List<PaymentDetailDTO> DetailList = new ArrayList<PaymentDetailDTO>();
 
 		int LGD_AMOUNT = 0;
 		String LGD_CUSTOM_USABLEPAY = "";
@@ -82,7 +82,7 @@ public class PurchaseJSON extends NewsbankServletBase {
 			String orderJson = request.getParameter("orderJson"); // json
 
 			UsageDAO usageDAO = new UsageDAO();
-			//List<UsageDTO> usageList = usageDAO.usageList(0); // 주문 가능 제품 전체
+			// List<UsageDTO> usageList = usageDAO.usageList(0); // 주문 가능 제품 전체
 			List<UsageDTO> usageList = usageDAO.usageList(); // 주문 가능 제품 전체 (사용용도: 일반회원+후불회원)
 
 			try {
@@ -94,7 +94,7 @@ public class PurchaseJSON extends NewsbankServletBase {
 					LGD_CUSTOM_USABLEPAY = jsonObj.get("LGD_CUSTOM_USABLEPAY").toString();
 					System.out.println("LGD_CUSTOM_USABLEPAY : " + jsonObj.get("LGD_CUSTOM_USABLEPAY"));
 				}
-				
+
 				if (jsonObj.get("orderType") != null) {
 					ORDER_TYPE = jsonObj.get("orderType").toString();
 				}
@@ -116,9 +116,9 @@ public class PurchaseJSON extends NewsbankServletBase {
 						System.out.println("" + (i + 1) + "usage : " + tempObj.get("usage"));
 						System.out.println("----------------------------");
 						System.out.println(LGD_AMOUNT + " : " + usageList.get(index - 1).getPrice());
-						
+
 						int usagePrice = getUsagePrice(usageList, index);
-						
+
 						if (AMOUNT != usagePrice) {
 							result = false;
 							message = "요청한 금액이 올바르지 않습니다.";
@@ -143,11 +143,11 @@ public class PurchaseJSON extends NewsbankServletBase {
 			// 로그인중 처리
 			MemberInfo = (MemberDTO) session.getAttribute("MemberInfo");
 			Date today = new Date();
-			Date tomorrow = new Date(new Date().getTime() + 60*60*24*1000);
+			Date tomorrow = new Date(new Date().getTime() + 60 * 60 * 24 * 1000);
 			String LGD_TIMESTAMP = DATE_TIME_FORMAT.format(today);// 타임스탬프
 			String LGD_CLOSEDATE = DATE_TIME_FORMAT.format(tomorrow);
-			
-			String LGD_BUYERIP =getClientIP(request); //request.getRemoteAddr(); // 구매자 아이피
+
+			String LGD_BUYERIP = getClientIP(request); // request.getRemoteAddr(); // 구매자 아이피
 			String LGD_BUYERID = MemberInfo.getId(); // 구매자 아이디
 			String LGD_BUYER = MemberInfo.getName(); // 구매자 명
 			String LGD_BUYEREMAIL = MemberInfo.getEmail(); // 구매자 이메일
@@ -158,7 +158,8 @@ public class PurchaseJSON extends NewsbankServletBase {
 			String CST_PLATFORM = "test";// LG유플러스 결제서비스 선택(test:테스트, service:서비스)
 			String CST_MID = "dahaminewsbank"; // LG유플러스로 부터 발급받으신 상점아이디를 입력하세요.
 			String LGD_MID = ("test".equals(CST_PLATFORM.trim()) ? "t" : "") + CST_MID; // 테스트 아이디는 't'를 제외하고 입력하세요.
-			String LGD_MERTKEY = "49bd4da3b4414d8396a591a1c9565bbd";// 상점MertKey(mertkey는 상점관리자 -> 계약정보 -> 상점정보관리에서 확인하실수 있습니다)
+			String LGD_MERTKEY = "49bd4da3b4414d8396a591a1c9565bbd";// 상점MertKey(mertkey는 상점관리자 -> 계약정보 -> 상점정보관리에서
+																	// 확인하실수 있습니다)
 
 			String LGD_ENCODING = "UTF-8";
 
@@ -178,15 +179,20 @@ public class PurchaseJSON extends NewsbankServletBase {
 			 */
 
 			String LGD_HASHDATA = md5(LGD_MID + LGD_OID + LGD_AMOUNT + LGD_TIMESTAMP + LGD_MERTKEY);
-			
-			//String SERVER_HOST  ="www.dev.newsbank.co.kr";//"localhost:8080";//www.newsbank.co.kr//www.dev.newsbank.co.kr
-			String SERVER_HOST  =request.getServerName();
+
+			// String SERVER_HOST
+			// ="www.dev.newsbank.co.kr";//"localhost:8080";//www.newsbank.co.kr//www.dev.newsbank.co.kr
+
+			String SERVER_HOST = request.getServerName();
+			if (request.getServerName().equalsIgnoreCase("localhost") || request.getServerName().equalsIgnoreCase("127.0.0.1")) {
+				SERVER_HOST += ":" + request.getServerPort();
+			}
 
 			/*
 			 * 가상계좌(무통장) 결제 연동을 하시는 경우 아래 LGD_CASNOTEURL 을 설정하여 주시기 바랍니다.
 			 */
 			// String LGD_CASNOTEURL = "http://www.newsbank.co.kr/Noteurl.Xpay";
-			String LGD_CASNOTEURL = "http://"+SERVER_HOST+"/Noteurl.Xpay";
+			String LGD_CASNOTEURL = "http://" + SERVER_HOST + "/Noteurl.Xpay";
 
 			/*
 			 * LGD_RETURNURL 을 설정하여 주시기 바랍니다. 반드시 현재 페이지와 동일한 프로트콜 및 호스트이어야 합니다. 아래 부분을 반드시
@@ -194,7 +200,7 @@ public class PurchaseJSON extends NewsbankServletBase {
 			 */
 			// String LGD_RETURNURL = "http://www.newsbank.co.kr/Returnurl.Xpay";// FOR
 			// MANUAL
-			String LGD_RETURNURL = "http://"+SERVER_HOST+"/Returnurl.Xpay";// FOR MANUAL
+			String LGD_RETURNURL = "http://" + SERVER_HOST + "/Returnurl.Xpay";// FOR MANUAL
 
 			LGD_DATA.put("CST_MID", CST_MID); // *LG U+와 계약시 설정한 상점아이디
 			LGD_DATA.put("LGD_VERSION", "JSP_Non-ActiveX_Standard"); // 사용 모듈 정보
@@ -209,10 +215,10 @@ public class PurchaseJSON extends NewsbankServletBase {
 			LGD_DATA.put("LGD_BUYER", LGD_BUYER); // * 구매자명
 			LGD_DATA.put("LGD_CUSTOM_SWITCHINGTYPE", "IFRAME"); //
 			LGD_DATA.put("LGD_PRODUCTINFO", LGD_PRODUCTINFO); // * 제품정보
-			
+
 			LGD_DATA.put("LGD_CASNOTEURL", LGD_CASNOTEURL); // 무통장입금 결과 수신페이지
 			LGD_DATA.put("LGD_CLOSEDATE", LGD_CLOSEDATE); // 무통장입금 마감시간
-			
+
 			LGD_DATA.put("LGD_OID", LGD_OID); // * 상점 거래번호
 			LGD_DATA.put("LGD_HASHDATA", LGD_HASHDATA); // *해쉬데이터 거래 위변조 방지
 			LGD_DATA.put("LGD_OSTYPE_CHECK", "P"); // LGD_OSTYPE_CHECK
@@ -232,14 +238,10 @@ public class PurchaseJSON extends NewsbankServletBase {
 			LGD_DATA.put("LGD_ENCODING", LGD_ENCODING);
 			LGD_DATA.put("LGD_ENCODING_NOTEURL", LGD_ENCODING);
 			LGD_DATA.put("LGD_ENCODING_RETURNURL", LGD_ENCODING);
-			
-			LGD_DATA.put("ORDER_TYPE", ORDER_TYPE);
-			
-			
-			
-			
 
-			//결제 정보 요청 저장
+			LGD_DATA.put("ORDER_TYPE", ORDER_TYPE);
+
+			// 결제 정보 요청 저장
 			PaymentManageDTO payment = new PaymentManageDTO();
 			payment.setMember_seq(MemberInfo.getSeq());
 			payment.setLGD_BUYER(LGD_BUYER);
@@ -254,7 +256,7 @@ public class PurchaseJSON extends NewsbankServletBase {
 			payment.setLGD_PAYDATE(LGD_TIMESTAMP);
 			payment.setLGD_PAYTYPE(LGD_CUSTOM_USABLEPAY);
 			PaymentDAO paymentDAO = new PaymentDAO(); // 회원정보 연결
-			int paymentManage_seq = paymentDAO.insertPaymentManage(payment,DetailList);
+			int paymentManage_seq = paymentDAO.insertPaymentManage(payment, DetailList);
 			System.out.println(paymentManage_seq);
 			if (paymentManage_seq > 0) {
 				// 등록 성공
@@ -270,17 +272,17 @@ public class PurchaseJSON extends NewsbankServletBase {
 		response.getWriter().print(json);
 
 	}
-	
+
 	// usageList_seq에 대한 price 반환
 	public static int getUsagePrice(List<UsageDTO> usageList, int index) {
 		int price = 0;
-		
-		for(UsageDTO usage : usageList) {
-			if(usage.getUsageList_seq() == index) {
+
+		for (UsageDTO usage : usageList) {
+			if (usage.getUsageList_seq() == index) {
 				price = usage.getPrice();
 			}
 		}
-		
+
 		return price;
 	}
 
@@ -288,7 +290,8 @@ public class PurchaseJSON extends NewsbankServletBase {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
@@ -328,27 +331,25 @@ public class PurchaseJSON extends NewsbankServletBase {
 			return null;
 		}
 	}
-	
-	 public String getClientIP(HttpServletRequest request) {
 
-	     String ip = request.getHeader("X-FORWARDED-FOR"); 
-	     
-	     if (ip == null || ip.length() == 0) {
-	         ip = request.getHeader("Proxy-Client-IP");
-	     }
+	public String getClientIP(HttpServletRequest request) {
 
-	     if (ip == null || ip.length() == 0) {
-	         ip = request.getHeader("WL-Proxy-Client-IP");  // 웹로직
-	     }
+		String ip = request.getHeader("X-FORWARDED-FOR");
 
-	     if (ip == null || ip.length() == 0) {
-	         ip = request.getRemoteAddr() ;
-	     }
-	     
-	     return ip;
+		if (ip == null || ip.length() == 0) {
+			ip = request.getHeader("Proxy-Client-IP");
+		}
 
-	 }
+		if (ip == null || ip.length() == 0) {
+			ip = request.getHeader("WL-Proxy-Client-IP"); // 웹로직
+		}
 
+		if (ip == null || ip.length() == 0) {
+			ip = request.getRemoteAddr();
+		}
 
+		return ip;
+
+	}
 
 }
