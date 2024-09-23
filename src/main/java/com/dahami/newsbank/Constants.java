cd331ed3b372f94c0a0d14338015f627210150ce
gkwlstn2001@dahami.com <gkwlstn2001@dahami.com>
2024-09-23 11:07:19 +0900

    240923 1.0.17 태그푸시
    1.0.17 태그푸시

--- src/main/java/com/dahami/newsbank/Constants.java
+++ src/main/java/com/dahami/newsbank/Constants.java
@@ -25,7 +25,7 @@ public class Constants {
 	/** 서비스에 따른 JSP_BASEPATH 구분 */
 	public static final String JSP_BASHPATH;
 	/** 테스트를 위해서 세팅하는 값 */
-	public static final boolean MEDIA_INCLUDE_TEST = Boolean.parseBoolean(System.getProperty("MEDIA_INCLUDE_TEST", "true"));
+	public static final boolean MEDIA_INCLUDE_TEST = Boolean.parseBoolean(System.getProperty("MEDIA_INCLUDE_TEST", "false"));
 	public static final String[] ADMISSION_TYPE = new String[] {
 		"N"
 	};
